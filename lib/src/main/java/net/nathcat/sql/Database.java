f4752f99844e8599ddaec3efe82bf13ad8f19ccd
Nathan Baines <nathan.baines@nathcat.net>
2026-02-03 21:33:51 +0000

    Simple database and typesafe query class

---
+++ lib/src/main/java/net/nathcat/sql/Database.java
@@ -0,0 +1,57 @@
+package net.nathcat.sql;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import com.mysql.cj.jdbc.exceptions.CommunicationsException;
+
+public class Database {
+  private Connection conn;
+  private final DBConfig config;
+
+  public Database(DBConfig config) {
+    this.config = config;
+  }
+
+  /**
+   * Connect to the configured database. See {@link DBConfig} for configuration.
+   */
+  private void connect() throws SQLException {
+    conn = DriverManager.getConnection(config.uri, config.username, config.password);
+  }
+
+  /**
+   * Create a new query on this connection
+   *
+   * @param sql SQL form of the query
+   * @throws SQLException
+   */
+  public Query newQuery(String sql) throws SQLException {
+    try {
+      return new Query(conn.prepareStatement(sql));
+    } catch (CommunicationsException e) {
+      connect();
+      return newQuery(sql);
+    }
+  }
+
+  /**
+   * Execute a query which should return a result set.
+   *
+   * @param q The query to execute
+   */
+  public ResultSet select(Query q) throws SQLException {
+    q.execute();
+    return q.getResultSet();
+  }
+
+  /**
+   * Execute a query which should not return a result set.
+   *
+   * @param q The query
+   */
+  public void update(Query q) throws SQLException {
+    q.execute();
+  }
+}
---
+++ lib/src/main/java/net/nathcat/sql/Query.java
@@ -0,0 +1,66 @@
+package net.nathcat.sql;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class Query {
+  private final PreparedStatement stmt;
+
+  protected Query(PreparedStatement stmt) {
+    this.stmt = stmt;
+  }
+
+  /**
+   * Get the setter method on {@link PreparedStatement} for a given type
+   *
+   * @param c The type
+   * @throws NoSuchMethodException Thrown if the method cannot be found, or there
+   *                               is no method for the given type.
+   */
+  private <T> Method getSetter(Class<T> c) throws NoSuchMethodException {
+    if (c == Integer.class) {
+      return PreparedStatement.class.getMethod("setInt", Integer.class, Integer.class);
+    } else if (c == String.class) {
+      return PreparedStatement.class.getMethod("setString", Integer.class, String.class);
+    } else if (c == Boolean.class) {
+      return PreparedStatement.class.getMethod("setBoolean", Integer.class, Boolean.class);
+    } else {
+
+      throw new NoSuchMethodException("No setter found for type " + c.getName());
+    }
+  }
+
+  /**
+   * Set a parameter in the query
+   *
+   * @param paramIndex The index of the parameter, starting from 1
+   * @param c          The class type of the parameter
+   * @param v          The parameter value
+   * @throws NoSuchMethodException Thrown if the setter cannot be found for the
+   *                               given type
+   */
+  public <T> Query set(int paramIndex, Class<T> c, T v) throws NoSuchMethodException {
+    try {
+      getSetter(c).invoke(stmt, paramIndex, v);
+    } catch (InvocationTargetException | IllegalAccessException e) {
+      throw new RuntimeException(e);
+    }
+
+    return this;
+  }
+
+  public void execute() throws SQLException {
+    stmt.execute();
+  }
+
+  public ResultSet getResultSet() throws SQLException {
+    return stmt.getResultSet();
+  }
+
+  public void close() throws SQLException {
+    stmt.close();
+  }
+}
